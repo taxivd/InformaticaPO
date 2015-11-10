e7d33c7605202db4a9a01892760392f7206fce08
Max <117233@WS-M3060.lekenlinge.local>
2015-11-10 09:14:46 +0100

    Verbeteringen

--- login.php
+++ login.php
@@ -4,19 +4,19 @@
 	if( isSet( $_POST['email'] ) && isSet( $_POST['wachtwoord'] ) ){
 		include ("functies.php"); 
 		dbconnect();
-		$mail = strtolower( $_POST['email'] );
+		$mail = strtolower( $_POST['email'] ); //zet alle letters in de email om naar kleine letters
 		$query = "SELECT * FROM Gebruikers WHERE email = '" . $mail . "' AND wachtwoord = '" . $_POST['wachtwoord'] . "'"; //Kijk of de email en het wachtwoord in de database staan
 		$result = mysql_query( $query );
 		
 		if ($result == true){ 
-			$numRows = mysql_num_rows($result);
+			$numRows = mysql_num_rows($result); 
 			$row = mysql_fetch_array($result);
-			if($numRows == 1){
-				$_SESSION['login'] = true;
-				$_SESSION['email'] = $row['email'];
+			if($numRows == 1){ //kijk of wat je gevonden hebt in de database in één rij staat
+				$_SESSION['login'] = true; //zet de login op true
+				$_SESSION['email'] = $row['email']; //zet het email in de rij
 			} else { 
-				$_SESSION['login'] = false;
-				$_SESSION['email'] = "";
+				$_SESSION['login'] = false; //zet de login op false
+				$_SESSION['email'] = ""; //maak de $_SESSION['email'] leeg
 			}
 		}
 	}else{ //als er niks is ingevuld, moet je terug naar de inlogpagina gaan
--- registreer.php
+++ registreer.php
@@ -1,12 +1,12 @@
 <?php
 	//Kijk of het wachtwoord en het controle wachtwoord hetzelfde zijn
 	if( $_POST['wachtwoord'] == $_POST['Controle'] ){
-		include ("functies.php");
+		include ("functies.php"); //maak verbinding met de database
 		dbconnect();
-		$mail = strtolower( $_POST['email'] );
+		$mail = strtolower( $_POST['email'] ); //zet alle letters in de email om naar kleine letters
 		$query = "INSERT INTO Gebruikers(email, wachtwoord) VALUES('". $mail ."','".$_POST['wachtwoord']."')"; //zet het email en het wachtwoord in de database
 		
-		$result = mysql_query( $query ); 
+		$result = mysql_query( $query );
 		
 		if ($result == false){ //als er een fout wordt gemaakt, kan je terug gaan naar de registratiepagina
 			echo "Er is iets fout gegaan:" . mysql_error();
