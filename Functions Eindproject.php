1407e9169a50f4d4e7f367b8d3a2393854f4cc20
NoPorter <117330@WS-M3059.lekenlinge.local>
2015-11-19 11:57:34 +0100

    hoi

--- Functions Eindproject.php
+++ Functions Eindproject.php
@@ -1,5 +1,5 @@
 <?php
-function registreerLeerling( $naam, $lln ){
+function registreerLeerling( $naam, $leerlingNummer ){
 	$query = "INSERT INTO Leerling( naam, leerlingNummer) VALUES('" . $naam . "', " . $leerlingNummer . ");";
 }
 function registreerDocent( $naam, $afkorting ){
@@ -18,6 +18,17 @@ function maakToets( $periode, $soort, $weeknr, $toetsWeek, $duur, $toetsWijze, $
 	$query = "INSERT INTO Toets( periode, soort, weeknr, toetsWeek, duur, toetsWijze, lokaalType, herkansbaar, RAP, DTW. ED, opmerking, hulpmiddelenSchool, hulpmiddelenThuis, aanpasbaar, docent_id) VALUES('" . $periode . "', " . $soort . ", " . $weeknr . " ," . $toetsWeek . ", " . $duur . " ," . $toetsWijze . ", " . $lokaalType . " ," . $herkansbaar . ", " . $RAP . " ," . $DTW . ", " . $ED . " ," . $opmerking . ", " . $hulpmiddelenSchool . " ," . $hulpmiddelenThuis . ", " . $aanpasbaar . " ," . $docent_id . " );";
 }
 
+function registreerBeheerder( $naam, $wachtwoord ){
+	$query = "INSERT INTO Beheerder( afkorting, vak) VALUES('" . $afkorting . "', " . $vak . ");";
+}
+
+function registreerCommissaris( $afkorting, $vak ){
+	$query = "INSERT INTO Commissaris( afkorting, vak) VALUES('" . $afkorting . "', " . $vak . ");";
+}
+
+function maakProfiel( $profiel ){
+	$query = "INSERT INTO Profiel( profiel ) VALUE('" . $profiel . "');";
+}
 
 
 //registreerLeerling( 'Bob', 123456 );
