0b5679e31c1e44b5f210d9abdcb91806eee6d785
Max <117233@WS-M3060.lekenlinge.local>
2015-12-17 12:20:02 +0100

    Verbeteringen

--- pta.php
+++ pta.php
@@ -1,4 +1,6 @@
-<!doctype html>
+<?php
+session_start();
+?><!doctype html>
 <html>
 	<head>
 		<link rel="stylesheet" href="pta.css" /> <!-- koppeling css bestand -->
@@ -12,23 +14,31 @@
 			<input class="btn" type="submit" value="Uitloggen"/> <!-- Knop om door te gaan naar login.php -->		
 		</form>
 		<?php
-			function PTAVoorbeeld(){
-				include('Functions Eindproject.php');
-				if( $vakken = vindVakkenVanLeerling( 'code' ) ){
+			include('Functions Eindproject.php');
+			if( $vakken = vindVakkenVanLeerling( $_SESSION['lln'] ) ){
 				foreach( $vakken as $vak ){
 					echo "<h1>" . $vak['vakNaam'] . "</h1>";
 			
 					if( $toetsen = vindToetsenVanVak( $vak['vakCode'] ) ){
 						foreach( $toetsen as $toets ){
+							echo "<p>" . $toets['toetsNaam'] . "</p>";
+							echo "<p>" . $toets['periode'] . "</p>";
+							echo "<p>" . $toets['soort'] . "</p>";
+							echo "<p>" . $toets['weeknr'] . "</p>";
+							echo "<p>" . $toets['toetsWeek'] . "</p>";
 							echo "<p>" . $toets['duur'] . "</p>";
-							}
+							echo "<p>" . $toets['toetsWijze'] . "</p>";
+							echo "<p>" . $toets['lokaalType'] . "</p>";
+							echo "<p>" . $toets['herkansbaar'] . "</p>";
+							echo "<p>" . $toets['RAP'] . "</p>";
+							echo "<p>" . $toets['DTW'] . "</p>";
+							echo "<p>" . $toets['ED'] . "</p>";
+
+
 						}
 					}
 				}
 			}
-		
 		?>
-		
-			
 	</body>
 </html>
