a0c2b06ce43f974f81b2b4283a3edae080a021d8
Max <117233@WS-M3060.lekenlinge.local>
2016-01-12 08:51:48 +0100

    Kleine wijzigingen

--- registreer.php
+++ registreer.php
@@ -1,6 +1,14 @@
 <?php
 	//Kijk of het wachtwoord en het controle wachtwoord hetzelfde zijn
-	if( $_POST['Wachtwoord'] == $_POST['Controle']){		
+	if( $_POST['Wachtwoord'] == $_POST['Controle']){
+	
+		$vakken = array();
+		foreach( $_POST as $key => $value ){
+			if( isSet( $key[4] ) && substr( $key, 0, 4 ) == 'vak_' ){
+				$vakken[] = substr( $key, 4 ) ;
+			}
+		}
+		
 		include ("functies.php"); //maak verbinding met de database
 		dbconnect();
 		
@@ -12,6 +20,14 @@
 			echo "Er is iets fout gegaan:" . mysql_error();
 			header('Location: Registratie.php');
 		} else {
+			foreach( $vakken as $vak ){
+				$klaas = "INSERT INTO VakGebruiker(vakCode, gebruikerCode) VALUES('". $vak . "', '". $_POST['lln'] . "')"; 
+				$piet = mysql_query( $klaas );
+		
+				if ($piet == false){ //als er een fout wordt gemaakt, kan je terug gaan naar de registratiepagina
+					echo "Er is iets fout gegaan:" . mysql_error();
+				}
+			}
 			header('Location: Inlogpagina.html');
 			echo 'Je bent succesvol geregistreerd';
 		}
